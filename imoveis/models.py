4b0b7684bfc168db122f6c055db19263f4015f72
Alecsander2005 <167083540+Alecsander2005@users.noreply.github.com>
2026-05-07 09:13:26 -0300

    resolvendo erro das imagens

--- imoveis/models.py
+++ imoveis/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.conf import settings
+from cloudinary.models import CloudinaryField
+
 
 class Imovel(models.Model):
-
 
     FINALIDADE_CHOICES = [
         ('venda', 'Venda'),
@@ -14,8 +15,7 @@ class Imovel(models.Model):
     CIDADE_CHOICES = [
         ('João Pessoa', 'João Pessoa'),
         ('Mediaçoes', 'Mediações'),
-        ]
-    
+    ]
 
     STATUS_CHOICES = [
         ('pronto', 'Pronto'),
@@ -25,18 +25,19 @@ class Imovel(models.Model):
     ]
 
     TIPO_CHOICES = [
-        ('casa', 'casa'),
-        ('apartamento', 'apartamento'),
-        ('terreno', 'terreno'),
-        ('comercial', 'comercial'),
-        ('cobertura', 'cobertura'),
-        ('flat', 'flat'),
-        ('condominio fechado', 'condominio fechado'),
-        ]
+        ('casa', 'Casa'),
+        ('apartamento', 'Apartamento'),
+        ('terreno', 'Terreno'),
+        ('comercial', 'Comercial'),
+        ('cobertura', 'Cobertura'),
+        ('flat', 'Flat'),
+        ('condominio fechado', 'Condomínio Fechado'),
+    ]
 
     titulo = models.CharField(max_length=255)
     descricao = models.TextField()
     preco = models.DecimalField(max_digits=10, decimal_places=2)
+
     status = models.CharField(max_length=20, choices=STATUS_CHOICES)
     cidade = models.CharField(max_length=100, choices=CIDADE_CHOICES)
     bairro = models.CharField(max_length=100)
@@ -44,30 +45,50 @@ class Imovel(models.Model):
     quartos = models.IntegerField()
     banheiros = models.IntegerField()
     vagas_garagem = models.IntegerField()
+
     metragem = models.DecimalField(max_digits=10, decimal_places=2)
+
     finalidade = models.CharField(max_length=20, choices=FINALIDADE_CHOICES)
-    tipo = models.CharField(max_length=20, choices=TIPO_CHOICES)
-    imagem_principal = models.ImageField(upload_to='imoveis/', blank=True, null=True)
+    tipo = models.CharField(max_length=30, choices=TIPO_CHOICES)
+
+    imagem_principal = CloudinaryField(
+        'image',
+        blank=True,
+        null=True
+    )
 
     criado_em = models.DateTimeField(auto_now_add=True)
 
     def link_whatsapp(self):
         try:
             numero = settings.WHATSAPP_NUMBER
+
             if not numero:
                 return None
-            mensagem = f"Olá, tenho interesse no imóvel '{self.titulo}' listado no seu site. Poderia me fornecer mais informações?"
+
+            mensagem = (
+                f"Olá, tenho interesse no imóvel "
+                f"'{self.titulo}' listado no seu site."
+            )
+
             return f"https://wa.me/{numero}?text={mensagem}"
+
         except AttributeError:
             return None
-    
+
     def __str__(self):
         return self.titulo
-    
+
+
 class Imagem(models.Model):
-    imovel = models.ForeignKey(Imovel, related_name='imagens', on_delete=models.CASCADE)
-    imagem = models.ImageField(upload_to='imoveis/')
+
+    imovel = models.ForeignKey(
+        Imovel,
+        related_name='imagens',
+        on_delete=models.CASCADE
+    )
+
+    imagem = CloudinaryField('image')
 
     def __str__(self):
         return f"Imagem do imóvel: {self.imovel.titulo}"
-    
